2d8d80deef5aafdc4690a68dd2428b0c3f7bea56
Bruno <bruno.rubim924@gmail.com>
2023-05-11 12:01:12 -0300

    creating inscryptoin-decide page

---
+++ Inscryption/Inscryption-Decide.js
@@ -0,0 +1,13 @@
+const playerCards = [
+	'stoat',
+	'riversnapper',
+	'grizzly',
+	'wolf',
+];
+
+let buttonRandomCard1 = documetn.queryselector('#randomCard1')
+function randomCard(cardGroup){
+	document.queryselector('');
+	Math.random();
+}
+buttonRandomCard1.onclick = randomCard(playerCards);
