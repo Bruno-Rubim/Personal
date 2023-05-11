12f58e06333c4a5972c7256e34ca16fd4c47546c
Bruno <bruno.rubim924@gmail.com>
2023-05-11 14:50:54 -0300

    changes on Inscryption

--- Inscryption/Inscryption-Decide.js
+++ Inscryption/Inscryption-Decide.js
@@ -5,9 +5,16 @@ const playerCards = [
 	'wolf',
 ];
 
-let buttonRandomCard1 = document.querySelector('#first');
+let serie = document.querySelector('.series');
+
 function randomCard(cardGroup){
-	let cardName = Math.random()*cardGroup.length;
-	this.sibling > img.src = "./images/pixel-" + cardName + ".png"
+	let i = Math.random()*cardGroup.length;
+	i = Math.floor(i);
+	console.log(i);
+	let cardName = cardGroup[i];
+	serie.querySelector('.cardImage').src = "./images/pixel-" + cardName + ".png"
+
+	//this.sibling > img.src = "./images/pixel-" + cardName + ".png"
 }
-buttonRandomCard1.onclick = randomCard(playerCards);
+
+serie.querySelector('.first').onclick = randomCard(playerCards);
