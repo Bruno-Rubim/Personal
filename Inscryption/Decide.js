6f70ddc25fa7e3814e25bcb76426a12feb4b3624
Bruno <bruno.rubim924@gmail.com>
2023-05-11 23:09:49 -0300

    mobile translation attempt

--- Inscryption/Decide.js
+++ Inscryption/Decide.js
@@ -10,7 +10,9 @@ function handleButton1 (){
 }
 
 function handleButton2 (){
-	document.querySelector('#rand2 img').src = randomSelect(leshyCards);
+	document.querySelector('#rand2img1').src = randomSelect(playerCards);
+	document.querySelector('#rand2img2').src = randomSelect(playerCards);
+	document.querySelector('#rand2img3').src = randomSelect(playerCards);
 }
 
 function handleButton3 (){
@@ -19,3 +21,4 @@ function handleButton3 (){
 
 document.querySelector('#rand1 button').onclick = handleButton1;
 document.querySelector('#rand2 button').onclick = handleButton2;
+document.querySelector('#rand3 button').onclick = handleButton3;
