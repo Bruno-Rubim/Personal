ebe88a3b00057308d5035a202adb5ebbb595418d
Bruno <bruno.rubim924@gmail.com>
2023-05-12 09:49:00 -0300

    wip

--- Inscryption/Decide.js
+++ Inscryption/Decide.js
@@ -5,14 +5,26 @@ function randomSelect(cardList){
 	return "./images/pixel-" + cardList[i] + ".png"
 }
 
+function noRepeat(card1, card2, cardList){
+	while (card1 == card2){
+		card2 = randomSelect(cardList); 
+	}
+	return card2;
+}
+
 function handleButton1 (){
 	document.querySelector('#rand1 img').src = randomSelect(playerCards);
 }
 
 function handleButton2 (){
-	document.querySelector('#rand2img1').src = randomSelect(playerCards);
-	document.querySelector('#rand2img2').src = randomSelect(playerCards);
-	document.querySelector('#rand2img3').src = randomSelect(playerCards);
+	let img1 = document.querySelector('#rand2img1');
+	let img2 = document.querySelector('#rand2img2');
+	let img3 = document.querySelector('#rand2img3');
+	img1.src = randomSelect(playerCards);
+	img2.src = randomSelect(playerCards);
+	img3.src = randomSelect(playerCards);
+	img2.src = noRepeat(img1.src, img2.src, playerCards);
+	img3.src = noRepeat(img2.src, img3.src, playerCards);
 }
 
 function handleButton3 (){
